23ff05f7c495cf688ddfc59a9054bc1a0adb17da
Tim Jacomb <timjacomb1@gmail.com>
2023-01-06 11:04:37 +0000

    DTSPO-11878 Add postgres db for pact broker

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Jenkins Infrastructure for CFT platform
 
 Creates the resources that CFT Jenkins requires
+
+Including:
+- A Postgres database for the pact broker
---
+++ components/jenkins/pact-database.tf
@@ -0,0 +1,39 @@
+data "azurerm_client_config" "this" {}
+
+module "ctags" {
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = var.environment
+  product      = var.product
+  builtFrom    = var.builtFrom
+  expiresAfter = "2023-01-01"
+}
+
+locals {
+  pact_broker_component = "pact-broker"
+}
+
+module "postgresql" {
+  source = "git::https://github.com/hmcts/terraform-module-postgresql-flexible.git?ref=master"
+  env    = var.env
+
+  product       = var.product
+  name          = "cft-${local.pact_broker_component}"
+  component     = local.pact_broker_component
+  business_area = "cft"
+
+  pgsql_databases = [
+    {
+      name : "pact"
+    }
+  ]
+
+  # Set your PostgreSQL version, note AzureAD auth requires version 12 (and not 11 or 13 currently)
+  pgsql_version = "14"
+
+  # The ID of the principal to be granted admin access to the database server, should be the principal running this normally
+  admin_user_object_id = data.azurerm_client_config.this.object_id
+
+  enable_read_only_group_access = false
+
+  common_tags = var.common_tags
+}
