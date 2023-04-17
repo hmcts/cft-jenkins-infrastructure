fa36928550fe4873135bc025dd737368bdb7b055
Matt Slater <72858053+msl8r@users.noreply.github.com>
2023-04-17 11:26:56 +0100

    use existing var

--- components/jenkins/init.tf
+++ components/jenkins/init.tf
@@ -18,5 +18,5 @@ provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "postgres_network"
-  subscription_id            = var.aks_subscription_id
+  subscription_id            = var.subscription_id
 }
