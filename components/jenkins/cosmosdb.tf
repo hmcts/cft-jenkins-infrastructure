60510fa84b5ea5c51eb0f439cf81df46cd43704f
Viktoriya Bogdanova <viktoriyabogdanova@jpvft6l5c4.home>
2023-05-16 15:46:15 +0100

    Automate Cosmosdb

--- components/jenkins/cosmosdb.tf
+++ components/jenkins/cosmosdb.tf
@@ -3,14 +3,6 @@
 #   suffix = var.env == "ptlsbox" ? "-sbox" : ""
 # }
 
-module "tags" {
-  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment  = lower(var.env)
-  product      = var.product
-  builtFrom    = var.builtFrom
-  expiresAfter = "3000-01-01"
-}
-
 locals {
   suffix = var.env == "ptlsbox" ? "sandbox" : ""
 }
