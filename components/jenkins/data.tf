6624939bca4404c12273e8369f0a6ac1bd0d34ab
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-05-25 11:11:00 +0100

    dtspo-13631: Jenkins IAC-permissions
    + Added terraform config for using role assignment resources.

--- components/jenkins/data.tf
+++ components/jenkins/data.tf
@@ -8,6 +8,19 @@ data "azurerm_resource_group" "jenkins_key_vault_rg" {
   name = var.key_vault_rg
 }
 
+data "azurerm_resource_group" "mi" {
+  name = "managed-identities-${var.env}-rg"
+}
+
+data "azurerm_resource_group" "acr_rg" {
+  provider = azurerm.acr
+  name     = "rpe-acr-prod-rg"
+}
+
+data "azurerm_subscription" "sub" {
+  subscription_id = data.azurerm_client_config.current.subscription_id
+}
+
 data "azurerm_subnet" "postgres" {
   name                 = "postgresql"
   resource_group_name  = "cft-${var.env}-network-rg"
---
+++ components/jenkins/role.tf
@@ -0,0 +1,34 @@
+resource "azurerm_user_assigned_identity" "usermi" {
+  resource_group_name = data.azurerm_resource_group.mi.name
+  location            = var.location
+  name                = "jenkins-${var.env}-mi"
+  tags                = module.tags.common_tags
+}
+
+resource "azurerm_role_assignment" "miroles" {
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.usermi.principal_id
+}
+
+resource "azurerm_role_assignment" "subidcontributer" {
+
+  for_each             = data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.ptl : local.ptlsbox
+  scope                = "/subscriptions/${each.value}"
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.usermi.principal_id
+}
+
+resource "azurerm_role_assignment" "subiduseraccessadmin" {
+
+  for_each             = data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.ptl : local.ptlsbox
+  scope                = "/subscriptions/${each.value}"
+  role_definition_name = "User Access Administrator"
+  principal_id         = azurerm_user_assigned_identity.usermi.principal_id
+}
+
+resource "azurerm_role_assignment" "hmctsacrpull" {
+  scope                = data.azurerm_resource_group.acr_rg.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.usermi.principal_id
+}
