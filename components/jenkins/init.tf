6a7f9d2d25c7023267a93911c13617937acbdb95
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-05-25 16:53:36 +0100

    Add provider

--- components/jenkins/init.tf
+++ components/jenkins/init.tf
@@ -27,3 +27,9 @@ provider "azurerm" {
   alias                      = "cosmosdb"
   subscription_id            = var.cosmos_subscription_id
 }
+
+provider "azurerm" {
+  subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
+  alias           = "acr"
+  features {}
+}
