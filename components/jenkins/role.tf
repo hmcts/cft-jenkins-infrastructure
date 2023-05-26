7cefb2a115d34499f597ff2a2cc61ac35838c3aa
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-05-26 14:29:28 +0100

    Change variables to include correct locals

--- components/jenkins/role.tf
+++ components/jenkins/role.tf
@@ -1,3 +1,17 @@
+locals {
+  ptl = {
+
+    stg  = "74dacd4f-a248-45bb-a2f0-af700dc4cf68",
+    test = "3eec5bde-7feb-4566-bfb6-805df6e10b90",
+    demo = "c68a4bed-4c3d-4956-af51-4ae164c1957c",
+    ithc = "ba71a911-e0d6-4776-a1a6-079af1df7139",
+    prod = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c",
+    dev  = "867a878b-cb68-4de5-9741-361ac9e178b6"
+  }
+  ptlsbox = {
+    sbox = "a8140a9e-f1b0-481f-a4de-09e2ee23f7ab"
+  }
+}
 
 resource "azurerm_user_assigned_identity" "usermi" {
   resource_group_name = var.mi_rg
@@ -14,7 +28,7 @@ resource "azurerm_role_assignment" "miroles" {
 
 resource "azurerm_role_assignment" "subidcontributer" {
 
-  for_each             = data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.suffix.ptl : local.suffix.ptlsbox
+  for_each             = data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.ptl : local.ptlsbox
   scope                = "/subscriptions/${each.value}"
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
@@ -22,7 +36,7 @@ resource "azurerm_role_assignment" "subidcontributer" {
 
 resource "azurerm_role_assignment" "subiduseraccessadmin" {
 
-  for_each             = data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.suffix.ptl : local.suffix.ptlsbox
+  for_each             = data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.ptl : local.ptlsbox
   scope                = "/subscriptions/${each.value}"
   role_definition_name = "User Access Administrator"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
