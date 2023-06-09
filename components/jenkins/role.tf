932137d1df9cb0030f53dd27d812a8fdc24f5176
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-06-09 15:23:04 +0100

    Change scope

--- components/jenkins/role.tf
+++ components/jenkins/role.tf
@@ -6,25 +6,25 @@ resource "azurerm_user_assigned_identity" "usermi" {
 }
 
 resource "azurerm_role_assignment" "miroles" {
-  scope                = data.azurerm_client_config.current.subscription_id
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
 
 resource "azurerm_role_assignment" "subidcontributer" {
-  scope                = data.azurerm_client_config.current.subscription_id
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
 
 resource "azurerm_role_assignment" "subiduseraccessadmin" {
-  scope                = data.azurerm_client_config.current.subscription_id
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "User Access Administrator"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
 
 resource "azurerm_role_assignment" "hmctsacrpull" {
-  scope                = data.azurerm_client_config.current.subscription_id
+  scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
