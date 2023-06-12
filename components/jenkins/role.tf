8e96aa6c6a033e5119bd4a0780a9e845824b8688
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-06-12 11:46:14 +0100

    Change to reference data instead of hardcoded string

--- components/jenkins/role.tf
+++ components/jenkins/role.tf
@@ -6,19 +6,19 @@ resource "azurerm_user_assigned_identity" "usermi" {
 }
 
 resource "azurerm_role_assignment" "miroles" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
 
 resource "azurerm_role_assignment" "subidcontributer" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
 
 resource "azurerm_role_assignment" "subiduseraccessadmin" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "User Access Administrator"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
