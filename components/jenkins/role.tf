0ed7c2f7c842bf98a4e015069d1b3fdf6f87e9b1
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-06-14 11:13:18 +0100

    Fix principle id path

--- components/jenkins/role.tf
+++ components/jenkins/role.tf
@@ -8,13 +8,13 @@ resource "azurerm_user_assigned_identity" "usermi" {
 resource "azurerm_role_assignment" "miroles" {
   scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "Contributor"
-  principal_id         = data.azuread_groups.dts_platform_operations.id
+  principal_id         = data.azuread_group.dts_platform_operations.id
 }
 
 resource "azurerm_role_assignment" "subidcontributer" {
   scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "Contributor"
-  principal_id         = data.azuread_groups.subidcontributer.id
+  principal_id         = data.azuread_group.subidcontributer.id
 }
 
 resource "azurerm_role_assignment" "subiduseraccessadmin" {
