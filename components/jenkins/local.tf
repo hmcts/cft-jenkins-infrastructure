4b52c4ef3b69bc1679cd2917f5734e53ab94ce4e
nicholas chang <nicholasc001@hotmail.com>
2024-07-25 09:45:29 +0100

    format code

--- components/jenkins/local.tf
+++ components/jenkins/local.tf
@@ -1,10 +1,10 @@
 locals {
-  mi_environment = var.env == "sbox" ? "sandbox" : var.env == "prod" ? "cftptl-intsvc" : var.env
+  mi_environment = var.env == "ptlsbox" ? "cftsbox-intsvc" : var.env == "ptl" ? "cftptl-intsvc" : var.env
   mi_cft = {
     # DTS-CFTSBOX-INTSVC
     cftsbox-intsvc = {
       subscription_id = "1497c3d7-ab6d-4bb7-8a10-b51d03189ee3"
-    }
+    },
     # DTS-CFTPTL-INTSVC
     cftptl-intsvc = {
       subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
