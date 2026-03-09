37e0e954e97fb24a96aaa411dcf1686f7409f71f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-03-09 15:15:25 +0000

    add ptlsbox

--- components/jenkins-agent-identities/cosmosdb.tf
+++ components/jenkins-agent-identities/cosmosdb.tf
@@ -1,8 +1,8 @@
 data "azurerm_cosmosdb_account" "pipeline_metrics" {
   provider = azurerm.cosmosdb
 
-  name                = var.env == "sbox" ? "sandbox-pipeline-metrics" : "pipeline-metrics"
-  resource_group_name = var.env == "sbox" ? "pipelinemetrics-database-sandbox" : "pipelinemetrics-database-prod"
+  name                = var.env == "sbox" || var.env == "ptlsbox" ? "sandbox-pipeline-metrics" : "pipeline-metrics"
+  resource_group_name = var.env == "sbox" || var.env == "ptlsbox" ? "pipelinemetrics-database-sandbox" : "pipelinemetrics-database-prod"
 }
 
 resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_data_contributor" {
