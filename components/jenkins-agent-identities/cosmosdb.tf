35e3c846cd32a067fc938233960d6771d70d05e5
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-03-09 14:36:13 +0000

    fix reference

--- components/jenkins-agent-identities/cosmosdb.tf
+++ components/jenkins-agent-identities/cosmosdb.tf
@@ -9,7 +9,7 @@ resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_data_contributor" {
   resource_group_name = data.azurerm_cosmosdb_account.pipeline_metrics.resource_group_name
   account_name        = data.azurerm_cosmosdb_account.pipeline_metrics.name
   # Cosmos DB Built-in Data Contributor
-  role_definition_id = "${azurerm_cosmosdb_account.cosmosdb.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
+  role_definition_id = "${data.azurerm_cosmosdb_account.pipeline_metrics.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
   principal_id       = local.principal_id
   scope              = data.azurerm_cosmosdb_account.pipeline_metrics.id
 }
