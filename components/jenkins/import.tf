6f41cbbee89046914c9a74d5cbc3cbc85fc766b7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-01 14:16:16 +0100

    remove var

--- components/jenkins/import.tf
+++ components/jenkins/import.tf
@@ -1,4 +1,9 @@
 import {
     to = azuread_group_member.jenkins
-    id = "e68123d3-c59d-4e95-96c7-62e3fa6f1a9f/member/${azurerm_user_assigned_identity.usermi.principal_id}"
+    id = "e68123d3-c59d-4e95-96c7-62e3fa6f1a9f/member/0292f26e-288e-4f5b-85fc-b99a53f0a2b1"
 }
+
+# import {
+#     to = azuread_group_member.jenkins
+#     id = "e68123d3-c59d-4e95-96c7-62e3fa6f1a9f/member/ca6d5085-485a-417d-8480-c3cefa29df31"
+# }
