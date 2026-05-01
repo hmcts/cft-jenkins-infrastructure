8b3602c1e1ee296433710a9dc48abe75958ff9af
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-01 14:00:20 +0100

    rename resource

--- components/jenkins/groups.tf
+++ components/jenkins/groups.tf
@@ -1,4 +1,4 @@
-resource "azuread_group_member" "name" {
+resource "azuread_group_member" "jenkins" {
   group_object_id  = data.azuread_group.directory_readers.object_id
   member_object_id = azurerm_user_assigned_identity.usermi.principal_id
 }
