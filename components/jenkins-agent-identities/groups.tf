60b03091fbfb1d89c9fa5d98b9bc160b0c094d5d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-01 10:05:18 +0100

    add acount

--- components/jenkins-agent-identities/groups.tf
+++ components/jenkins-agent-identities/groups.tf
@@ -1,4 +1,5 @@
 resource "azuread_group_member" "name" {
+  count            = var.create_identity ? 1 : 0
   group_object_id  = data.azuread_group.directory_readers.object_id
-  member_object_id = azurerm_user_assigned_identity.this.principal_id
+  member_object_id = azurerm_user_assigned_identity.this[count.index].principal_id
 }
