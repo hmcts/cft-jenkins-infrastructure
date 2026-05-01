90ff7b060a85412f7b8e092ca48c02e45c63b244
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-01 16:11:43 +0100

    make import work in each env

--- components/jenkins/import.tf
+++ components/jenkins/import.tf
@@ -1,9 +1,5 @@
 import {
-    to = azuread_group_member.jenkins
-    id = "e68123d3-c59d-4e95-96c7-62e3fa6f1a9f/member/0292f26e-288e-4f5b-85fc-b99a53f0a2b1"
+  for_each = local.group_members
+  to       = azuread_group_member.jenkins
+  id       = each.value
 }
-
-# import {
-#     to = azuread_group_member.jenkins
-#     id = "e68123d3-c59d-4e95-96c7-62e3fa6f1a9f/member/ca6d5085-485a-417d-8480-c3cefa29df31"
-# }
