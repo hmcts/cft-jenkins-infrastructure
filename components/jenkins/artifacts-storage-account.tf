3c885ed40b34fdb43fc135ea0619518a52258788
Daniel Alves <daniel.alves@justice.gov.uk>
2026-09-07 12:47:11 +0100

    feat: expire Jenkins cache after 30 days
    Refs DTSPO-34836

--- components/jenkins/artifacts-storage-account.tf
+++ components/jenkins/artifacts-storage-account.tf
@@ -26,6 +26,37 @@ resource "azurerm_storage_container" "job_cache" {
   container_access_type = "private"
 }
 
+data "azurerm_storage_account" "sandbox_job_cache" {
+  count = var.env == "ptlsbox" ? 1 : 0
+
+  provider            = azurerm.cache_storage
+  name                = "mgmtbuildlogstoresandbox"
+  resource_group_name = "mgmt-buildlog-store-sandbox"
+}
+
+resource "azurerm_storage_management_policy" "job_cache" {
+  count = contains(["ptl", "ptlsbox"], var.env) ? 1 : 0
+
+  provider           = azurerm.cache_storage
+  storage_account_id = var.env == "ptl" ? azurerm_storage_account.storage_account[0].id : data.azurerm_storage_account.sandbox_job_cache[0].id
+
+  rule {
+    name    = "delete-job-cache-after-30-days"
+    enabled = true
+
+    filters {
+      prefix_match = ["job-cache/"]
+      blob_types   = ["blockBlob"]
+    }
+
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 30
+      }
+    }
+  }
+}
+
 resource "azurerm_key_vault_secret" "job_cache_account_key" {
   count = var.env == "ptl" ? 1 : 0
 
