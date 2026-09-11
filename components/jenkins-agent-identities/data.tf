9918e5e2d1a2861b014850a3a74e5f961d677525
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-11 09:44:04 +0100

    add count to data resource

--- components/jenkins-agent-identities/data.tf
+++ components/jenkins-agent-identities/data.tf
@@ -18,6 +18,7 @@ data "azurerm_monitor_action_group" "slack_alerts" {
 }
 
 data "azurerm_key_vault" "cftptl_vault" {
+  count               = var.env == "prod" ? 1 : 0
   provider            = azurerm.cft-ptl
   name                = "cftptl-intsvc"
   resource_group_name = "core-infra-intsvc-rg"
