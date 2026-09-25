c9a63c38eb96f36b25086f06363409c0154489cb
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-25 09:53:22 +0100

    add new role assignment capability

--- components/jenkins-agent-identities/locals.tf
+++ components/jenkins-agent-identities/locals.tf
@@ -28,4 +28,9 @@ locals {
     substr(md5("Private DNS Zone Contributor:/subscriptions/${var.private_dns_subscription_id}/resourceGroups/${var.private_dns_resource_group_name}:${local.principal_id}"), 16, 4),
     substr(md5("Private DNS Zone Contributor:/subscriptions/${var.private_dns_subscription_id}/resourceGroups/${var.private_dns_resource_group_name}:${local.principal_id}"), 20, 12)
   )
+
+  rbac_admin_role_guids = {
+    for name, def in data.azurerm_role_definition.rbac_admin_role :
+    name => element(split("/", def.id), length(split("/", def.id)) - 1)
+  }
 }
